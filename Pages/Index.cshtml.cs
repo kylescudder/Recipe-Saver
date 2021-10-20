5292363119ef6e807c28af409cefea636436a7e2
Kyle Scudder <kyle.s@paloma.co.uk>
2021-10-20 14:27:16 +0100

    Added Sentry error logging

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,17 +1,17 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Recipe_Saver.Data;
 using Recipe_Saver.Models;
+using Sentry;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace Recipe_Saver.Pages
 {
+<<<<<<< Updated upstream
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
@@ -56,4 +56,46 @@ namespace Recipe_Saver.Pages
             }
         }
     }
+=======
+	public class IndexModel : PageModel
+	{
+		private readonly ILogger<IndexModel> _logger;
+		private readonly IRecipeData _recipeSaver;
+		public IndexModel(ILogger<IndexModel> logger, IRecipeData recipeSaver)
+		{
+			_logger = logger;
+			_recipeSaver = recipeSaver;
+		}
+		public List<RecipeList> Recipes { get; set; }
+		public NewRecipe NewRecipe { get; set; }
+		public async Task<ActionResult> OnGet()
+		{
+			try
+			{
+				Recipes = await _recipeSaver.GetRecipeList();
+				return Page();
+			}
+			catch (Exception er)
+			{
+				SentrySdk.CaptureException(er);
+				throw new Exception(er.ToString());
+			}
+		}
+		public async Task<IActionResult> OnPost()
+		{
+			try
+			{
+				_recipeSaver.AddRecipe(Request.Form["txtRecipeName"].ToString()
+						, Request.Form["txtRecipeLink"].ToString()
+						, Convert.ToDateTime(Request.Form["txtForWeekCommencing"]));
+				return RedirectToPage("./Index");
+			}
+			catch (Exception ex)
+			{
+				SentrySdk.CaptureException(ex);
+				throw new Exception(ex.ToString());
+			}
+		}
+	}
+>>>>>>> Stashed changes
 }
--- Program.cs
+++ Program.cs
@@ -1,26 +1,58 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Serilog;
+using Sentry;
 
 namespace Recipe_Saver
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var configuration = new ConfigurationBuilder()
+					.AddJsonFile("appsettings.json")
+					.Build();
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+			Log.Logger = new LoggerConfiguration()
+					.ReadFrom.Configuration(configuration)
+					.CreateLogger();
+			string version = configuration.GetSection("Version")["Major"] + "." + configuration.GetSection("Version")["Minor"] + "." + configuration.GetSection("Version")["Revision"];
+			using (SentrySdk.Init(o =>
+            {
+                o.Release = "recipe-saver@" + version;
+                o.Dsn = "https://11e5611b6d1b4e40b158608be2a0ca78@o1044877.ingest.sentry.io/6020699";
+                // When configuring for the first time, to see what the SDK is doing:
+                o.Debug = true;
+                // Set traces_sample_rate to 1.0 to capture 100% of transactions for performance monitoring.
+                // We recommend adjusting this value in production.
+                o.TracesSampleRate = 1.0;
+            }))
+			{
+				// App code goes here. Dispose the SDK before exiting to flush events.
+
+				try
+				{
+					SentrySdk.CaptureMessage("Appliation Starting Up");
+					CreateHostBuilder(args).Build().Run();
+				}
+				catch (Exception ex)
+				{
+					SentrySdk.CaptureException(ex);
+				}
+				finally
+				{
+					Log.CloseAndFlush();
+				}
+			}
+		}
+
+		public static IHostBuilder CreateHostBuilder(string[] args) =>
+				Host.CreateDefaultBuilder(args)
+						.ConfigureWebHostDefaults(webBuilder =>
+						{
+							webBuilder.UseStartup<Startup>();
+						});
+	}
 }
