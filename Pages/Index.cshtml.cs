e2bdb5d5a533d064c83e7c8220c77862a005a552
Kyle Scudder <kyle.s@paloma.co.uk>
2021-10-20 14:28:56 +0100

    Resolved merge conflict

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -11,52 +11,6 @@ using System.Threading.Tasks;
 
 namespace Recipe_Saver.Pages
 {
-<<<<<<< Updated upstream
-    public class IndexModel : PageModel
-    {
-        private readonly ILogger<IndexModel> _logger;
-        private readonly IRecipeData _recipeSaver;
-        public IndexModel(ILogger<IndexModel> logger, IRecipeData recipeSaver)
-        {
-            _logger = logger;
-            _recipeSaver = recipeSaver;
-        }
-        public List<RecipeList> Recipes { get; set; }
-        public NewRecipe NewRecipe { get; set; }
-        public async Task<ActionResult> OnGet()
-        {
-            Recipes = await _recipeSaver.GetRecipeList();
-            return Page();
-        }
-        public async Task<IActionResult> OnPost()
-        {
-            try
-            {
-                _recipeSaver.AddRecipe(Request.Form["txtRecipeName"].ToString()
-                    , Request.Form["txtRecipeLink"].ToString()
-                    , Convert.ToDateTime(Request.Form["txtForWeekCommencing"]));
-                return RedirectToPage("./Index");
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.ToString());
-            }
-        }
-        [HttpPost]
-        public async Task<IActionResult> OnPostArchive(IFormCollection collection)
-        {
-            try
-            {
-                _recipeSaver.ArchiveRecipe(Convert.ToInt32(Request.Form["RecipeListID"]));
-				return RedirectToPage("./Index", new { @redirect = "archive" });
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.ToString());
-            }
-        }
-    }
-=======
 	public class IndexModel : PageModel
 	{
 		private readonly ILogger<IndexModel> _logger;
@@ -85,7 +39,7 @@ namespace Recipe_Saver.Pages
 		{
 			try
 			{
-				_recipeSaver.AddRecipe(Request.Form["txtRecipeName"].ToString()
+				await _recipeSaver.AddRecipe(Request.Form["txtRecipeName"].ToString()
 						, Request.Form["txtRecipeLink"].ToString()
 						, Convert.ToDateTime(Request.Form["txtForWeekCommencing"]));
 				return RedirectToPage("./Index");
@@ -96,6 +50,19 @@ namespace Recipe_Saver.Pages
 				throw new Exception(ex.ToString());
 			}
 		}
+        [HttpPost]
+        public async Task<IActionResult> OnPostArchive(IFormCollection collection)
+        {
+            try
+            {
+                await _recipeSaver.ArchiveRecipe(Convert.ToInt32(Request.Form["RecipeListID"]));
+				return RedirectToPage("./Index", new { @redirect = "archive" });
+            }
+            catch (Exception ex)
+            {
+                SentrySdk.CaptureException(ex);
+                throw new Exception(ex.ToString());
+            }
+        }
 	}
->>>>>>> Stashed changes
 }
